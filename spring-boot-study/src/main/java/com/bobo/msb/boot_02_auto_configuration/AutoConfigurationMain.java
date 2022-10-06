236c7cbfd9ed3fffb32be3dfa50b2a41afaabdb2
lishenglun <wy183055@163.com>
2022-10-07 01:34:46 +0800

    成功编译spring boot

--- spring-boot-study/src/main/java/com/bobo/msb/boot_02_auto_configuration/AutoConfigurationMain.java
+++ spring-boot-study/src/main/java/com/bobo/msb/boot_02_auto_configuration/AutoConfigurationMain.java
@@ -1,10 +1,12 @@
 package com.bobo.msb.boot_02_auto_configuration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 自动装配的原理：通过Import导入spring.factories文件中的配置类，然后解析配置类，实现自动装配。
  */
+@SpringBootApplication
 public class AutoConfigurationMain {
 
 	/**
@@ -19,7 +21,7 @@ public class AutoConfigurationMain {
 	 * 3、需要写starter的场景：自己开发一个组件，需要整合到某个spring  boot开发的业务系统里面去，这个时候可以写一个starter
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(LabelSpringBootApplicationAnnotation.class,args);
+		SpringApplication.run(AutoConfigurationMain.class,args);
 	}
 
 }
