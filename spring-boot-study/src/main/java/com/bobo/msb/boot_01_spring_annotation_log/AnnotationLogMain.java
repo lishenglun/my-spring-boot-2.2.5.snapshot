0b9041c075a37c5883673f4508ce5f700d5bf2b9
lishenglun <wy183055@163.com>
2022-10-12 00:09:32 +0800

    @Indexed

--- spring-boot-study/src/main/java/com/bobo/msb/boot_01_spring_annotation_log/AnnotationLogMain.java
+++ spring-boot-study/src/main/java/com/bobo/msb/boot_01_spring_annotation_log/AnnotationLogMain.java
@@ -1,7 +1,7 @@
 package com.bobo.msb.boot_01_spring_annotation_log;
 
 /**
- * Spring版本中注解的发展历史
+ * 一、Spring版本中注解的发展历史
  *
  * 1、spring1.0
  *
@@ -30,9 +30,7 @@ package com.bobo.msb.boot_01_spring_annotation_log;
  * @Configuration
  * @ImportResource：实现Java配置类和xml配置的混合使用，来实现平稳过度。这样我们在启动spring容器的时候，就可以只引入一个配置类进行启动即可。
  * @Import
- * @ComponentScan：
- *
- * 在Spring3.1版之前，还不能够完全实现去XML配置，因为配置扫描路径我们还只能在XML配置文件中通过<component-scan>标签来实现，
+ * @ComponentScan：在Spring3.1版之前，还不能够完全实现去XML配置，因为配置扫描路径我们还只能在XML配置文件中通过<component-scan>标签来实现，
  * 在3.1版本到来的时候，提供了@ComponentScan，该注解的作用是替换掉<component-scan>标签，是注解编程很大的进步，也是Spring实现无配置文件的坚实基础
  *
  * 4、spring4.0
@@ -50,9 +48,8 @@ package com.bobo.msb.boot_01_spring_annotation_log;
  * 这里面检索和遍历的效率，随着java文件越来越多，效率就会越来越低，对系统启动的影响越来越大，所以@ComponentScan在启动时存在性能问题
  *
  * 我们程序有2个阶段，编译和运行。在编译的时候，我们的代码已经确定了，哪些类中有注解修饰，已经知道了，所以@Indexed就是在编译的时候，帮我们把已经有对应注解修饰的java类都收集起来，存储在一个文件里面去；
- * 当系统启动的时候，它就只需要去这一个索引文件里面找到要加载的java类的全路径即可，这样一来就把启动时的性能影响转接到编译时
- *
- * 当系统启动的时候，只需要加载这一个文件，通过一次IO，就找到了要注入容器中的这些Java类的全路径，就把扫描的过程给省略掉了，增加了系统启动速度
+ * 当系统启动的时候，只需要加载这一个索引文件，去这一个文件里面找到要加载的java类的全类路径限定名即可，这样一来就把启动时的性能影响转接到编译时，
+ * 通过一次IO，就找到了要注入容器中的这些Java类的全路径名，把扫描的过程给省略掉了，增加了系统启动速度
  *
  * 根据这个索引文件就可以找到所有要载入容器中的Java类。@Component里面内置了@Indexed，所以我们去添加组件注解的话，在spring5.0之后，编译的时候默认都会识别到要被扫描的Java文件，要生效只需要加上spring-context-indexer依赖即可
  */
